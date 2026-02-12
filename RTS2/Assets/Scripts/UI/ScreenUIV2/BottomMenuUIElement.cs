d11ec4630eabb0fcfe9fc2f1223dd183d73e78f7
RichardScott95 <richard.scott_1990@live.co.uk>
2026-02-12 21:02:27 +0000

    Selection mode gets highlighted when its active

--- RTS2/Assets/Scripts/UI/ScreenUIV2/BottomMenuUIElement.cs
+++ RTS2/Assets/Scripts/UI/ScreenUIV2/BottomMenuUIElement.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class BottomMenuUIElement : BaseUIElement
 {
-    public ButtonManager Construction, Selection, Rooms, World;
+    public Button Construction, Selection, Rooms, World;
     public BaseUIElement ConstructionElement, SelectionElement, RoomsElement, WorldElement;
     BaseUIElement currentlyDrawn;
     public BottomMenuMode Mode;
@@ -20,15 +20,28 @@ public class BottomMenuUIElement : BaseUIElement
 
     void InitButtons()
     {
-        Construction.gameObject.GetComponent<Button>().onClick.AddListener(()=>OpenUI(BottomMenuMode.Construction));
-        Selection.gameObject.GetComponent<Button>().onClick.AddListener(() => OpenUI(BottomMenuMode.Selection));
-        Rooms.gameObject.GetComponent<Button>().onClick.AddListener(() => OpenUI(BottomMenuMode.Rooms));
-        World.gameObject.GetComponent<Button>().onClick.AddListener(() => OpenUI(BottomMenuMode.World));
+        Construction.onClick.AddListener(()=>OpenUI(BottomMenuMode.Construction));
+        Selection.onClick.AddListener(() => OpenUI(BottomMenuMode.Selection));
+        Rooms.onClick.AddListener(() => OpenUI(BottomMenuMode.Rooms));
+        World.onClick.AddListener(() => OpenUI(BottomMenuMode.World));
 
+    }
+
+    void SelectButton(Button b)
+    {
+        b.transform.GetChild(3).GetComponent<CanvasGroup>().alpha = 1f;
+    }
+    void DeselectButton(Button b)
+    {
+        b.transform.GetChild(3).GetComponent<CanvasGroup>().alpha = 0f;
     }
 
     void OpenUI(BottomMenuMode modeToBe)
     {
+        DeselectButton(Construction);
+        DeselectButton(Selection); 
+        DeselectButton(Rooms);
+        DeselectButton(World);
         if (modeToBe == Mode)
         {
             if (currentlyDrawn != null)
@@ -52,18 +65,23 @@ public class BottomMenuUIElement : BaseUIElement
                     break;
                 case BottomMenuMode.Construction:
                     ConstructionElement.DrawUI();
+                    SelectButton(Construction);
                     currentlyDrawn = ConstructionElement;
                     break;
                 case BottomMenuMode.Selection:
                     SelectionElement.DrawUI();
+                    SelectButton(Selection);
                     currentlyDrawn = SelectionElement;
                     break;
                 case BottomMenuMode.Rooms:
                     RoomsElement.DrawUI();
+                    SelectButton(Rooms);
                     currentlyDrawn = RoomsElement;
                     break;
                 case BottomMenuMode.World:
-                    WorldElement.DrawUI();
+                    WorldElement?.DrawUI();
+                    SelectButton(World);
+
                     currentlyDrawn = WorldElement;
                     break;
             }
